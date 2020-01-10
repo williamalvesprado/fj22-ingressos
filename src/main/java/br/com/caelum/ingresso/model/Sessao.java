29d4b334765fedc297d4a1547e7d58eb11fd9735
William Prado <williamalvesprado>
2020-01-09 22:12:14 -0300

    Criado as classes para teste de insave de sessão

--- src/main/java/br/com/caelum/ingresso/model/Sessao.java
+++ src/main/java/br/com/caelum/ingresso/model/Sessao.java
@@ -2,12 +2,33 @@ package br.com.caelum.ingresso.model;
 
 import java.time.LocalTime;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+
+
+@Entity
 public class Sessao {
 	
+	@Id
+	@GeneratedValue
 	private Integer id;
 	private LocalTime horario;
+	
+	@ManyToOne
 	private Sala sala;
+	@ManyToOne
 	private Filme filme;
+	
+	/** 
+	 * @deprecated hibernate only
+	 */
+	
+	public Sessao() {
+	
+	}
 	
 	public Sessao(LocalTime  horario, Filme filme, Sala sala) {
 		this.horario = horario;
